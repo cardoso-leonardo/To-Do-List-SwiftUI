1137aef4f7bb3777e12950c769350e468bace265
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-06-20 15:02:30 -0300

    chore: folder structure and ignoring google plist

--- ToDoList/ToDoList/Other/ToDoListApp.swift
+++ ToDoList/ToDoList/Other/ToDoListApp.swift
@@ -5,10 +5,16 @@
 //  Created by Leonardo Cardoso on 20/06/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
