1eefbfc83c4a0e18653d4d8d2a82780f024b321c
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-06-20 14:40:35 -0300

    feat: first commit

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Leonardo Cardoso on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
